a47afed324f5bf50e2f62c729d775a5c2b461651
Maxim <Maxim@Lenny>
2015-03-16 16:58:15 +0100

    Initial Commit

---
+++ Konverter/src/konverter/Konverter.java
@@ -0,0 +1,24 @@
+package konverter;
+
+import java.io.IOException;
+
+
+public class Konverter {
+	
+	
+	  public static void main(String[] args) throws IOException{
+		  String export = "D:\\Uni\\Informatik\\Semester 9\\Bachelorarbeit\\IVUfleet\\";
+		  String output = "D:\\Uni\\Informatik\\Semester 9\\Bachelorarbeit\\Ausgabe2\\";
+		  
+//		  vehicleTransit vt = new vehicleTransit();
+		  network nw = new network();
+//		  transitSchedule ts = new transitSchedule();
+		  
+//		  vt.create(export, output);
+		  nw.create(export, output);
+//		  ts.create(export, output);
+	  }
+			  
+	
+		
+}
